ef9f15ce55cbfd98bbbaa700d7e2e2cc0f09ec6f
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2021-05-14 19:36:22 +0530

    Added example files (format only) WITHOUT parameters

---
+++ swarm_tasks/Examples/basic_tasks/surround.py
@@ -0,0 +1,36 @@
+print("Running foraging example 1\nUsing source files for package imports\nPARAMETERS:")
+import sys,os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'../../..')))
+print(sys.path)
+
+import swarm_tasks
+
+from swarm_tasks.simulation import simulation as sim
+from swarm_tasks.simulation import visualizer as viz
+	
+#Required modules
+import swarm_tasks.controllers.base_control as base_control
+from swarm_tasks.modules.dispersion import disp_field
+from swarm_tasks.modules.surround import surround_attractor
+
+
+#Initialize Simulation and GUI 
+s = sim.Simulation(env_name='rectangles')
+gui = viz.Gui(s)
+gui.show_env()
+
+while(1):
+	for b in s.swarm:
+		#Base control
+		cmd = base_control.base_control(b)
+		cmd+= base_control.obstacle_avoidance(b)
+		
+		#Behaviour
+		cmd+= disp_field(b)*0.05
+		cmd+=surround_attractor(b)
+		
+		#Execute
+		cmd.exec(b)
+		
+	gui.update()
