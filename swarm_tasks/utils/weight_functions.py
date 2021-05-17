c5e4f11bdba522cf9738e38130bdb2e1ddb34081
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2021-05-17 17:38:17 +0530

    Added periodic weight change strategies

--- swarm_tasks/utils/weight_functions.py
+++ swarm_tasks/utils/weight_functions.py
@@ -11,3 +11,11 @@ def linear_trunc(t, initial_val=0.0, final_val=1.0, rate=0.001):
 	lin = initial_val + rate*(final_val-initial_val)*t
 	min_lin, max_lin = min(initial_val, final_val), max(initial_val, final_val)
 	return max(min(lin, max_lin), min_lin)
+
+def sinusoidal(t, trough=0.0, crest=1.0, rate=0.01):
+	return trough + (crest-trough)*(np.sin(rate*t-np.pi/2)+1)/2
+
+def triangular(t, trough=0.0, crest=1.0, rate=0.001):
+	rel_t = t%int(1/rate)
+	change = 2*min((crest-trough)*rate*rel_t, crest-trough - (crest-trough)*rate*rel_t)
+	return trough + change
